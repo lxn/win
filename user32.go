6927d488bbe9ed03036f99263546a1d9685f495e
aqtata <aqtata@qq.com>
2016-11-29 14:23:26 +0800

    add WM_NCHITTEST message.

--- user32.go
+++ user32.go
@@ -875,6 +875,39 @@ const (
 	WM_CLIPBOARDUPDATE        = 0x031D
 )
 
+// WM_NCHITTEST message
+const (
+	HTERROR       = -2
+	HTTRANSPARENT = -1
+	HTNOWHERE     = 0
+	HTCLIENT      = 1
+	HTCAPTION     = 2
+	HTSYSMENU     = 3
+	HTGROWBOX     = 4
+	HTSIZE        = HTGROWBOX
+	HTMENU        = 5
+	HTHSCROLL     = 6
+	HTVSCROLL     = 7
+	HTMINBUTTON   = 8
+	HTMAXBUTTON   = 9
+	HTLEFT        = 10
+	HTRIGHT       = 11
+	HTTOP         = 12
+	HTTOPLEFT     = 13
+	HTTOPRIGHT    = 14
+	HTBOTTOM      = 15
+	HTBOTTOMLEFT  = 16
+	HTBOTTOMRIGHT = 17
+	HTBORDER      = 18
+	HTREDUCE      = HTMINBUTTON
+	HTZOOM        = HTMAXBUTTON
+	HTSIZEFIRST   = HTLEFT
+	HTSIZELAST    = HTBOTTOMRIGHT
+	HTOBJECT      = 19
+	HTCLOSE       = 20
+	HTHELP        = 21
+)
+
 // mouse button constants
 const (
 	MK_CONTROL  = 0x0008
